d2c567632428fb97927f6c8d1f1a38567282be3e
Joaquim Haas <joaquimnatelhaas@gmail.com>
2026-02-19 20:52:14 -0300

    Aula 76 finalizada - toString

---
+++ src/main/java/org/EstudoJava/JavaCore/Sobrescrita/domain/Anime.java
@@ -0,0 +1,22 @@
+package org.EstudoJava.JavaCore.Sobrescrita.domain;
+
+public class Anime extends Object{
+    private String nome;
+
+    @Override //Override para mudar o retorno
+    public String toString(){ //toString, uma função para ser visivel o que é/está acontecendo na classe
+        return "Anime " + this.nome; //
+    }
+
+    public Anime(String Nome){
+        this.nome = Nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
---
+++ src/main/java/org/EstudoJava/JavaCore/Sobrescrita/test/AnimeTest01.java
@@ -0,0 +1,11 @@
+package org.EstudoJava.JavaCore.Sobrescrita.test;
+
+import org.EstudoJava.JavaCore.Sobrescrita.domain.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+
+        Anime anime = new Anime("Fire Force");
+        System.out.println(anime);
+    }
+}
