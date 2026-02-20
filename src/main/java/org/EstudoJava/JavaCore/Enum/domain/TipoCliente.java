2f058a58ba09336747f036924eab386d53aea2e2
Joaquim Haas <joaquimnatelhaas@gmail.com>
2026-02-20 15:11:48 -0300

    Aula 81 finalizada - Construtores e atributos em Enumeração

--- src/main/java/org/EstudoJava/JavaCore/Enum/domain/TipoCliente.java
+++ src/main/java/org/EstudoJava/JavaCore/Enum/domain/TipoCliente.java
@@ -1,6 +1,17 @@
 package org.EstudoJava.JavaCore.Enum.domain;
 
 public enum TipoCliente {
-    PESSOA_FISICA,
-    PESSOA_JURIDICA
+    PESSOA_FISICA( 1, "Pessoa Fisica"),
+    PESSOA_JURIDICA(2, "Pessoa Juridica");
+
+    public final int VALOR;//ou private int valor;
+    public final String NOME;
+
+    TipoCliente(int Valor, String nomeRelatorio) {
+        this.VALOR = Valor;
+        this.NOME = nomeRelatorio;
+    }
+
+    //pode-se criar getters para quando for private tipoVar nomeVar;
+
 }
--- src/main/java/org/EstudoJava/JavaCore/Enum/test/ClienteTest01.java
+++ src/main/java/org/EstudoJava/JavaCore/Enum/test/ClienteTest01.java
@@ -2,19 +2,16 @@ package org.EstudoJava.JavaCore.Enum.test;
 
 import org.EstudoJava.JavaCore.Enum.domain.Cliente;
 import org.EstudoJava.JavaCore.Enum.domain.TipoCliente;
+import org.EstudoJava.JavaCore.Enum.domain.Cliente.TipoPagamento;
 
 public class ClienteTest01 {
     public static void main(String[] args) {
 
-        Cliente cliente1 = new Cliente("Katakuri", TipoCliente.PESSOA_JURIDICA);
-        Cliente cliente2 = new Cliente("Kata", TipoCliente.PESSOA_JURIDICA);
-        Cliente cliente3 = new Cliente("Toguro", TipoCliente.PESSOA_FISICA);
-        Cliente cliente4 = new Cliente("Tamandua", TipoCliente.PESSOA_FISICA);
+        Cliente cliente1 = new Cliente("Katakuri", TipoCliente.PESSOA_FISICA, Cliente.TipoPagamento.Debito);
+        Cliente cliente2 = new Cliente("Kata", TipoCliente.PESSOA_JURIDICA, Cliente.TipoPagamento.Credito);
 
         System.out.println(cliente1);
         System.out.println(cliente2);
-        System.out.println(cliente3);
-        System.out.println(cliente4);
 
     }
 }
