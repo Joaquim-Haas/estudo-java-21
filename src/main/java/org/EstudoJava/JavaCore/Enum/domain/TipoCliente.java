9205b505e86ef285f367eec7f7b844f16fe47bc7
Joaquim Haas <joaquimnatelhaas@gmail.com>
2026-02-20 16:41:13 -0300

    Aula 83 finalizada - Busca por atributos em enumeração

--- src/main/java/org/EstudoJava/JavaCore/Enum/domain/TipoCliente.java
+++ src/main/java/org/EstudoJava/JavaCore/Enum/domain/TipoCliente.java
@@ -4,12 +4,29 @@ public enum TipoCliente {
     PESSOA_FISICA( 1, "Pessoa Fisica"),
     PESSOA_JURIDICA(2, "Pessoa Juridica");
 
-    public final int VALOR;//ou private int valor;
-    public final String NOME;
+    private int valor;//ou public final int valor;
+    private String nomeRelatorio;
 
     TipoCliente(int Valor, String nomeRelatorio) {
-        this.VALOR = Valor;
-        this.NOME = nomeRelatorio;
+        this.valor = Valor;
+        this.nomeRelatorio = nomeRelatorio;
+    }
+
+    public static TipoCliente tipoClientePorNomeRelatorio(String nomeRelatorio){
+        for(TipoCliente tipoCliente : values()){
+            if(tipoCliente.getNomeRelatorio().equals(nomeRelatorio)){
+                return tipoCliente;
+            }
+        }
+        return null;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public String getNomeRelatorio() {
+        return nomeRelatorio;
     }
 
     //pode-se criar getters para quando for private tipoVar nomeVar;
--- src/main/java/org/EstudoJava/JavaCore/Enum/test/ClienteTest01.java
+++ src/main/java/org/EstudoJava/JavaCore/Enum/test/ClienteTest01.java
@@ -15,5 +15,12 @@ public class ClienteTest01 {
 
         System.out.println(TipoPagamento.DEBITO.calcularDesconto(100));
         System.out.println(TipoPagamento.CREDITO.calcularDesconto(100));
+
+        TipoCliente tipoCliente1 = TipoCliente.valueOf("PESSOA_FISICA");
+        System.out.println(tipoCliente1);
+        TipoCliente tipoCliente2 = TipoCliente.tipoClientePorNomeRelatorio("Pessoa Fisica");
+        System.out.println(tipoCliente2);
+        TipoCliente tipoCliente3 = TipoCliente.tipoClientePorNomeRelatorio("Pessoa Fisicaxxxx");
+        System.out.println(tipoCliente3);
     }
 }
