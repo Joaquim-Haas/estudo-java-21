24d61fc54483b8555f67d573733dbe289afb8ddb
Joaquim Haas <joaquimnatelhaas@gmail.com>
2026-03-13 13:58:06 -0300

    Aula 108 finalizada - Classe utilitaria String pt 01

---
+++ src/main/java/org/EstudoJava/JavaCore/String/test/StringTest01.java
@@ -0,0 +1,20 @@
+package org.EstudoJava.JavaCore.String.test;
+
+public class StringTest01 {
+    public static void main(String[] args) {
+        //String sao imutaveis
+        String nome = "Joaquim"; //String constant pool
+        String nome2 = "Joaquim";
+
+        System.out.println(nome);
+        nome = nome.concat(" Haas");
+        System.out.println(nome);
+        System.out.println(nome == nome2);
+
+        String nome3 = new String("Joaquim"); // 1 - variavel de referencia
+                                                     // 2 - objeto tipo string
+                                                     // 3 - string no pool de string
+        System.out.println(nome2 == nome3);
+        System.out.println(nome2 == nome3.intern());
+    }
+}
