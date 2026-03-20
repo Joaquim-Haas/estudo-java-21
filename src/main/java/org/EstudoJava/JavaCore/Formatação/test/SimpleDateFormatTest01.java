68f1571a9d77134320975ba883765c45e2b2884a
Joaquim Haas <joaquimnatelhaas@gmail.com>
2026-03-20 12:09:20 -0300

    Aula 118 finalizada - Classe Utilitária SimpleDateFormat

---
+++ src/main/java/org/EstudoJava/JavaCore/Formatação/test/SimpleDateFormatTest01.java
@@ -0,0 +1,23 @@
+package org.EstudoJava.JavaCore.Formatação.test;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class SimpleDateFormatTest01 {
+    public static void main(String[] args) {
+
+        String pattern = "'Brasil - Paraná, Ponta Grossa' dd 'de' MMMM 'de' yyyy";
+        //String pattern = "yyyy.MM.dd G 'at' HH:mm:ss z";
+        SimpleDateFormat Sdf = new SimpleDateFormat(pattern);
+
+        System.out.println(Sdf.format(new Date()));
+
+        try{
+            Sdf.parse("Brasil - Paraná, Ponta Grossa 20 de março de 2026");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
